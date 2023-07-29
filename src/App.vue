8269fc80b737305d5ccc3c83572f3c5e75370463
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-29 16:17:00 -0400

    starting files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div>
+    <Calculator msg="Vite + Vue" />
+  </div>
+</template>
+
---
+++ src/components/Calculator.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>my calculator</h1>
+  </div>
+</template>
+
