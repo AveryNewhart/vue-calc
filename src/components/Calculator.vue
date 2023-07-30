c26f523bc63705cc2d38b32b4c38025ba005ee8b
AveryNewhart <newhartreeceavery@gmail.com>
2023-07-30 19:43:29 -0400

    adding comments

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Main Div for the whole page -->
   <div class="p-3 max-w-400px mx-auto bg-gray-900" style="margin: 50px auto;">
 
     <!-- Calculator Result -->
@@ -26,40 +27,60 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Calculator',
+  // allows passing data from parent components to this component
   props: {
     msg: String,
   },
   data() {
     return {
+      // The current value displayed on the calculator screen
       calcVal: '' as string,
+      // An array containing calculator button values
       calcBtns: ['C', '%', '=', '+', 7, 8, 9, '-', 4, 5, 6, '*', 1, 2, 3, '/', 0, '.'] as (string)[],
+      // The selected operator (+, -, *, or /) for performing calculations
       operators: null as string | null,
+      // The previous value stored before selecting an operator
       prevCalcVal: '' as string,
     };
   },
   methods: {
+    // Function to handle button clicks and perform actions
     action(button: string) {
+      // If the clicked button is a number or decimal point
       if (!isNaN(Number(button)) || button === '.') {
+        // Append the button value to the current calculator value
         this.calcVal += button;
       }
 
+      // If the clicked button is the clear button ('C')
       if (button === 'C') {
+        // Clear the calculator value to reset the input
         this.calcVal = '';
       }
 
+      // If the clicked button is the percentage button ('%')
       if (button === '%') {
+        // Convert the value to a percentage
         this.calcVal = (Number(this.calcVal) / 100).toString();
       }
 
+      // If the clicked button is an operator
       if (['/', '+', '-', '*'].includes(button)) {
+        // Store the selected operator
         this.operators = button;
+        // Save the current value as the previous value
         this.prevCalcVal = this.calcVal;
+        // Clear the calculator value for the next input
         this.calcVal = '';
       }
 
+      // If the clicked button is the equals button ('=')
       if (button === '=') {
+        // Evaluate the expression and update the calculator value
         this.calcVal = eval(this.prevCalcVal + this.operators + this.calcVal).toString();
+        // Reset the previous value
         this.prevCalcVal = '';
+        // Reset the selected operator
         this.operators = null;
       }
     },
@@ -67,6 +88,8 @@ export default defineComponent({
 });
 </script>
 
+
+<!-- STYLES -->
 <style scoped>
 .bg-gray-900 {
   background: rgb(77, 0, 77);
